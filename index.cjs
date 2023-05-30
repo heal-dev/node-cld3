ff71f940d7bd6179c33a6984632f322bf4a35c55
vdeturckheim <vlad2t@hotmail.com>
2023-05-30 18:05:28 +0200

    ready for publish

--- index.cjs
+++ index.cjs
@@ -1,6 +1,22 @@
 /***
 * Copyright (c) 2023-2023 - All Rights Reserved
 */
-const cld3 = require('bindings')('cld3.node');
+const os = require('os');
+const path = require('path');
+
+let file = '';
+switch (os.platform()) {
+    case 'darwin':
+        file = 'cld3-macos.node';
+        break;
+    case 'linux':
+        file = 'cld3-linux.node';
+        break;
+}
+if (!file) {
+    throw new Error('cld3 is not supported on this platform.');
+}
+
+const cld3 = require(path.join(__dirname, 'addons', file));
 
 module.exports.getLanguage = cld3.getLanguage;
